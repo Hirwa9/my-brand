6c95ca8958e49be1513e6f532c58b8c4053c5b1a
hirwa9 <hirwawilly9@gmail.com>
2025-03-30 19:12:16 +0200

    Implement/complete a stable and user friendly send message functionality

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useState } from "react";
 import { motion } from "framer-motion";
+import toast, { Toaster } from 'react-hot-toast';
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -17,50 +19,121 @@ import {
 
 // Data
 import { info } from "@/app/data";
+import { sendContactForm } from "@/lib/api";
+import { PiArrowsClockwise } from "react-icons/pi";
 
 const Contact = () => {
+
+    // Form data
+    const [formData, setFormData] = useState({
+        firstname: "",
+        lastname: "",
+        email: "",
+        phone: "",
+        service: "Web Development",
+        message: "",
+    });
+
+    // Form states
+    const [isLoading, setIsLoading] = useState(false);
+
+    const resetForm = () => {
+        setFormData({
+            firstname: "",
+            lastname: "",
+            email: "",
+            phone: "",
+            service: "Web Development",
+            message: "",
+        });
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    // Submit message
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        try {
+            setIsLoading(true);
+            const loadingToast = toast.loading("Sending message...", {
+                style: {
+                    backgroundColor: '#71b3b3',
+                    color: '#fff',
+                },
+            });
+
+            const data = await sendContactForm(formData);
+            console.log(data);
+
+            if (data.success) {
+                // Show success message
+                toast.success("Thank you for reaching out! I'll review the message and respond to you shortly.", {
+                    duration: 6000,
+                    style: {
+                        backgroundColor: '#008000',
+                        color: '#fff',
+                    },
+                });
+                // Dismiss loading toast
+                if (loadingToast) toast.dismiss(loadingToast);
+            }
+            resetForm();
+        } catch (error) {
+            console.error(error);
+            toast.error("Sorry! Something went wrong. Please try again.", {
+                style: {
+                    backgroundColor: '#b30000',
+                    color: '#fff',
+                },
+            });
+        }
+        finally {
+            setIsLoading(false);
+        }
+    };
+
     return (
         <motion.section
             initial={{ opacity: 0 }}
             animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' } }}
             className="py-6"
         >
+            <Toaster />
             <div className="container mx-auto">
                 <div className="flex flex-col xl:flex-row gap-[1.875rem]">
                     {/* Form */}
                     <div className="xl:w-[54%] order-2 xl:order-none">
-                        <form action="" method="post" onSubmit={(e) => e.preventDefault()}
-                            className="flex flex-col gap-6 p-6 md:p-10 bg-white/5 rounded-xl border border-white/20 shadow-md shadow-black/20"
-                        >
+                        <form onSubmit={handleSubmit} className="flex flex-col gap-6 p-6 md:p-10 bg-white/5 rounded-xl border border-white/20 shadow-md shadow-black/20">
                             <h3 className="text-4xl text-accent">Let&#39;s work together</h3>
-                            <p className="text-white/60">
-                                Get in touch to discuss your project needs and bring your ideas to life.
-                            </p>
-                            {/* Inputs */}
+                            <p className="text-white/60">Get in touch to discuss your project needs and bring your ideas to life.</p>
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                                <Input type="firstname" placeholder="Firstname" />
-                                <Input type="lastname" placeholder="Lastname" />
-                                <Input type="email" autoComplete="true" placeholder="Email Address" />
-                                <Input type="phone" autoComplete="true" placeholder="Phone Number" />
+                                <Input name="firstname" value={formData.firstname} onChange={handleChange} type="text" placeholder="Firstname" />
+                                <Input name="lastname" value={formData.lastname} onChange={handleChange} type="text" placeholder="Lastname" />
+                                <Input name="email" value={formData.email} onChange={handleChange} type="email" placeholder="Email Address" />
+                                <Input name="phone" value={formData.phone} onChange={handleChange} type="text" placeholder="Phone Number" />
                             </div>
-                            {/* Select */}
-                            <Select defaultValue="est">
+                            <Select value={formData.service} onValueChange={(value) => setFormData({ ...formData, service: value })}>
                                 <SelectTrigger className="w-full">
                                     <SelectValue placeholder="Select a service" />
                                 </SelectTrigger>
                                 <SelectContent>
                                     <SelectGroup>
-                                        <SelectLabel className="">Select a service</SelectLabel>
-                                        <SelectItem value="est">Web Development</SelectItem>
-                                        <SelectItem value="cst">Mobile Development</SelectItem>
-                                        <SelectItem value="seo">SEO</SelectItem>
+                                        <SelectLabel>Select a service</SelectLabel>
+                                        <SelectItem value="Web Development">Web Development</SelectItem>
+                                        <SelectItem value="Mobile Development">Mobile Development</SelectItem>
+                                        <SelectItem value="SEO">SEO</SelectItem>
                                     </SelectGroup>
                                 </SelectContent>
                             </Select>
-                            {/* Textarea */}
-                            <Textarea placeholder="Your message" className="resize-none h-32" />
-                            {/* Button */}
-                            <Button size="md" className="max-w-40">Send message</Button>
+                            <Textarea name="message" value={formData.message} onChange={handleChange} placeholder="Your message" className="resize-none h-32" />
+                            <Button size="md" type="submit" disabled={isLoading ? true : undefined} className={`md:max-w-[50%] px-5 ${isLoading ? 'disabled text-white transition-colors duration-300' : ''} disabled:opacity-75 disabled:pointer-events-none`}>
+                                <span className="me-3">Send message</span> {isLoading && (
+                                    <span className="animate-spin"><PiArrowsClockwise /></span>
+                                )}
+                            </Button>
                         </form>
                     </div>
                     {/* Info */}
