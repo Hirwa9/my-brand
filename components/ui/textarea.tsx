38373db5449c5a709c3ccb3a9f5dc715db69d981
hirwa9 <hirwawilly9@gmail.com>
2025-03-30 19:09:55 +0200

    Use primary(dark) background on textarea inputs

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -7,7 +7,7 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
     <textarea
       data-slot="textarea"
       className={cn(
-        "border-white/10 placeholder:text-white/60 focus-visible:border-accent focus-visible:ring-accent/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm selection:bg-white/20 dark:bg-input/30 file:text-foreground placeholder:text-muted-foreground disabled:pointer-events-none",
+        "border-white/10 placeholder:text-white/60 focus-visible:border-accent focus-visible:ring-accent/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-primary px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm selection:bg-white/20 dark:bg-input/30 file:text-foreground placeholder:text-muted-foreground disabled:pointer-events-none",
         className
       )}
       {...props}
