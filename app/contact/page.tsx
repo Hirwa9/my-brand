3b4947787b3d1824a57cea3da2ebaee366e7fdfa
hirwa9 <hirwawilly9@gmail.com>
2025-04-03 17:56:34 +0200

    Prevent invalid/incomplete contact form submission

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -21,8 +21,12 @@ import {
 import { info } from "@/app/data";
 import { sendContactForm } from "@/lib/api";
 import { PiArrowsClockwise } from "react-icons/pi";
+import useNotify from "@/hooks/useToast";
 
 const Contact = () => {
+
+    // Notify helper
+    const { notify } = useNotify();
 
     // Form data
     const [formData, setFormData] = useState({
@@ -51,6 +55,29 @@ const Contact = () => {
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
+
+    function handleInvalid(e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) {
+        e.preventDefault();
+        // Gets the name of the field
+        const inputName = (e.target as HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement).name;
+
+        switch (inputName) {
+            case "firstname":
+                notify({ message: "Please provide your name so that we can address you properly." });
+                break;
+            case "email":
+                notify({ message: "Your Email Address is missing or incorrect." });
+                break;
+            case "phone":
+                notify({ message: "Looks like your Phone Number is is missing or incorrect!" });
+                break;
+            case "message":
+                notify({ message: "Your message box is empty.", type: 'error' });
+                break;
+            default:
+                notify({ message: "There's an issue with your input. Please check and try again!" });
+        }
+    }
 
     // Submit message
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -120,10 +147,10 @@ const Contact = () => {
                             <h3 className="text-4xl text-accent">Let&#39;s work together</h3>
                             <p className="text-white/60">Get in touch to discuss your project needs and bring your ideas to life.</p>
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                                <Input name="firstname" value={formData.firstname} onChange={handleChange} type="text" placeholder="Firstname" />
+                                <Input name="firstname" value={formData.firstname} onChange={handleChange} type="text" placeholder="Firstname" required onInvalid={handleInvalid} />
                                 <Input name="lastname" value={formData.lastname} onChange={handleChange} type="text" placeholder="Lastname" />
-                                <Input name="email" value={formData.email} onChange={handleChange} type="email" placeholder="Email Address" />
-                                <Input name="phone" value={formData.phone} onChange={handleChange} type="text" placeholder="Phone Number" />
+                                <Input name="email" value={formData.email} onChange={handleChange} type="email" placeholder="Email Address" required onInvalid={handleInvalid} />
+                                <Input name="phone" value={formData.phone} onChange={handleChange} type="text" placeholder="Phone Number" required onInvalid={handleInvalid} />
                             </div>
                             <Select value={formData.service} onValueChange={(value) => setFormData({ ...formData, service: value })}>
                                 <SelectTrigger className="w-full">
@@ -138,7 +165,7 @@ const Contact = () => {
                                     </SelectGroup>
                                 </SelectContent>
                             </Select>
-                            <Textarea name="message" value={formData.message} onChange={handleChange} placeholder="Your message" className="resize-none h-32" />
+                            <Textarea name="message" value={formData.message} onChange={handleChange} placeholder="Your message" className="resize-none h-32" required onInvalid={handleInvalid} />
                             <Button size="md" type="submit" disabled={isLoading ? true : undefined} className={`md:max-w-[50%] px-5 ${isLoading ? 'disabled text-white transition-colors duration-300' : ''} disabled:opacity-75 disabled:pointer-events-none`}>
                                 <span className="me-3">Send message</span> {isLoading && (
                                     <span className="animate-spin"><PiArrowsClockwise /></span>
