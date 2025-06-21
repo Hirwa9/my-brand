40d0188a3a44151d5ffb40e6b0504f7bf98941b2
hirwa9 <hirwawilly9@gmail.com>
2025-06-21 22:45:55 +0200

    Move plus-sign logic to CountUp's formatting function

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -15,11 +15,13 @@ const Stats = () => {
                         return (
                             <div key={index} className="flex-1 md:flex items-center justify-start gap-4">
                                 <div className="text-4xl xl:text-6xl font-extrabold text-nowrap">
-                                    <CountUp end={item.num} duration={5} delay={2} formattingFn={(val: number): string => convertCount(val)} />{
-                                        [
+                                    <CountUp end={item.num} duration={5} delay={2} formattingFn={(val: number): string => convertCount({
+                                        num: val,
+                                        usePlusSign: [
                                             "Code commits",
                                             "Technologies mastered",
-                                        ].includes(item.text) ? "+" : ""
+                                        ].includes(item.text)
+                                    })} />{
                                     }
                                 </div>
                                 <p className={`min-w-[80px] ${item.text.length < 15 ? 'max-w-[100px]' : 'max-w-[150px]'} leading-snug text-white/80`}>
