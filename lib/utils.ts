7c0a64f59d5266b9c809e1c1928f38422928d0dc
hirwa9 <hirwawilly9@gmail.com>
2025-06-21 22:43:55 +0200

    Use optional plus-sign suffix on formated value

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,14 +6,17 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 /**
- * Convert big number count to K, M, B, etc.
+ * Format big number count to K, M, B, etc.
  * @param num
+ * @param usePlusSign?
  * @returns converted string. Eg: 1000 -> 1K
  */
-export function convertCount(num: number): string {
-  if (num < 1000) return num.toString()
-  if (num < 1000000) return `${(num / 1000).toFixed(1)}K`
-  if (num < 1000000000) return `${(num / 1000000).toFixed(1)}M`
-  if (num < 1000000000000) return `${(num / 1000000000).toFixed(1)}B`
-  return `${(num / 1000000000000).toFixed(1)}T`
+export function convertCount({
+  num, usePlusSign = false
+}: { num: number, usePlusSign?: boolean }): string {
+  let result = num.toString()
+  if (num >= 1000 && num < 1000000) result = `${(num / 1000).toFixed(1)}K`
+  if (num >= 1000000 && num < 1000000000) result = `${(num / 1000000).toFixed(1)}M`
+  if (num >= 1000000000 && num < 1000000000000) result = `${(num / 1000000000).toFixed(1)}B`
+  return usePlusSign ? `${result}+` : result;
 }
